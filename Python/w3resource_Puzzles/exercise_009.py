a5d91290db9f472d51fce827fc73cbfc91d40708
Caden <141542344+PorkKnox@users.noreply.github.com>
2024-12-17 14:22:04 -0600

    Started 9 😭

---
+++ Python/w3resource_Puzzles/exercise_009.py
@@ -0,0 +1,38 @@
+"""
+    https://www.w3resource.com/python-exercises/puzzles/index.php#:~:text=9.%20Four%20Distinct,the%20sample%20solution
+    9. Four Distinct Values Non-Consecutive
+
+    Write a Python program to find a list of integers containing exactly four distinct values, such that no integer repeats twice consecutively among the first twenty entries.
+    
+    Input:
+    [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+    Output:
+    True
+    
+    Input:
+    [1, 2, 3, 3, 1, 2, 3, 3, 1, 2, 3, 3, 1, 2, 3, 3]
+    Output:
+    False
+    
+    Input:
+    [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
+    Output:
+    False
+    
+    Click me to see the sample solution
+    https://www.w3resource.com/python-exercises/puzzles/python-programming-puzzles-9.php
+"""
+
+numList_1 = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+numList_2 = [1, 2, 3, 3, 1, 2, 3, 3, 1, 2, 3, 3, 1, 2, 3, 3]
+numList_3 = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+def DistinctValues(inputList : list):
+    
+    
+    
+    return
+
+DistinctValues(numList_1)
+DistinctValues(numList_2)
+DistinctValues(numList_3)
