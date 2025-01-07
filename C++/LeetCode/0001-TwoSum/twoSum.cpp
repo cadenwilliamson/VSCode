7fa71a620eaa601ba53da167f5ee6a3a346b9d44
Caden <141542344+PorkKnox@users.noreply.github.com>
2025-01-07 14:40:48 -0600

    Start twoSum but I'm failing leetcode ;(

---
+++ C++/LeetCode/0001-TwoSum/twoSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+vector<int> numList_01 = {2,7,11,15};
+vector<int> numList_02 = {3,2,4};
+vector<int> numList_03 = {3,3};
+
+
+vector<int> twoSum(vector<int> &nums, int target) {
+    for (int i = 0; i < target; i++);
+        return target;
+};
+
+
+int main() {
+    twoSum(numList_01, 3);
+
+    return 0;
+}
