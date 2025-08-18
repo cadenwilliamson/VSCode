bcbb94b5636f7de4d5b830f9364195659f967c2e
Caden <141542344+PorkKnox@users.noreply.github.com>
2025-08-18 09:10:47 -0500

    Update

---
+++ Python/pride.py
@@ -0,0 +1,14 @@
+from rich import print
+from time import sleep
+
+colors = ["red", "green", "blue", "yellow", "magenta", "cyan"]
+
+for i in range(0, 3):
+    print("[red] Hello Pride!")
+    print("[green] Hello Pride!")
+    print("[blue] Hello Pride!")
+    print("[yellow] Hello Pride!")
+    print("[magenta] Hello Pride!")
+    print("[cyan] Hello Pride!")
+    
+    sleep(0.5)
